6070a86bf5119546314c4158ed7cb2413d727e4d
192210086 <141109929+192210086@users.noreply.github.com>
2023-11-21 13:02:38 +0530

    Add files via upload

---
+++ 20. Finding whether the given integer is odd or even.c program.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int number = 7;  
+
+    
+    printf("Name: venkata kiran Y\n");
+    printf("Reg No: 192210086\n");
+
+    
+    if (number % 2 == 0) {
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+
+    return 0;
+}
+
